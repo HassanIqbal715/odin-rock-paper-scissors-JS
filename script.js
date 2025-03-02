ccc238101da5b9a1b534f05fa2c9b50eeeeccc29
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-03-02 15:36:06 +0500

    Updated playRound function: Added main game logic

--- script.js
+++ script.js
@@ -28,6 +28,35 @@ function playRound(humanChoice, computerChoice) {
     else {
         console.log("Scissors!");
     }
+
+    if (humanChoice === computerChoice) {
+        console.log("It's a Draw! Nobody gets the points...");
+        return 2;
+    }
+    else if (humanChoice === 0 && computerChoice === 1) {
+        console.log("Computer Wins! +1 for Computer...");
+        return 0;
+    }
+    else if (humanChoice === 0 && computerChoice === 2) {
+        console.log("You Win! +1 for You!");
+        return 1;
+    }
+    else if (humanChoice === 1 && computerChoice === 0) {
+        console.log("You Win! +1 for You!");
+        return 1;
+    }
+    else if (humanChoice === 1 && computerChoice === 2) {
+        console.log("Computer Wins! +1 for Computer...");
+        return 0;
+    }
+    else if (humanChoice === 2 && computerChoice === 0) {
+        console.log("Computer Wins! +1 for Computer...");
+        return 0;
+    }
+    else {
+        console.log("You Win! +1 for You!");
+        return 1;
+    }
 }
 
 let humanScore = 0;
