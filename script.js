ab3091bbb609b2b2419b73dbf9b7e11d8b5f036f
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-03-02 15:19:24 +0500

    Created variables for human score and computer score

--- script.js
+++ script.js
@@ -6,5 +6,8 @@ function getHumanChoice() {
     return parseInt(prompt("Enter your choice from 0 to 2. (0 = rock) (1 = paper) (2 = scissors): "));
 }
 
+let humanScore = 0;
+let computerScore = 0;
+
 console.log(getComputerChoice());
 console.log(getHumanChoice());
