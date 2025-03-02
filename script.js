97ed53629bc49973a992a8758585e19de4d6d472
HassanIqbal715 <158496042+HassanIqbal715@users.noreply.github.com>
2025-03-02 15:18:35 +0500

    Finished creating getHumanChoice function. It can now take inputs and return whatever is entered

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-
+    return parseInt(prompt("Enter your choice from 0 to 2. (0 = rock) (1 = paper) (2 = scissors): "));
 }
 
 console.log(getComputerChoice());
+console.log(getHumanChoice());
